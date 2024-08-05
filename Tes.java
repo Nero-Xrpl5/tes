308d18efebe3970e419be3ef5909593e87e7c721
Nero-Xrpl5 <sulungdinero@gmail.com>
2024-08-05 14:21:36 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,14 @@
+/**
+ * Tes
+ */
+public class Tes {
+ public static void main(String[] args) {
+        String nama = "Dinero Sulung Suseno";
+        String kelas = "X RPL 5";
+        String kota = "Jember";
+        String suku = "Jawa";
+        String bahasa = "Jawa Pandalungan";
+        String salam = "Terimakasih";
+        System.out.print("Halo, nama saya " + nama + " dari Kelas " + kelas + " dari kota " + kota + ", Suku saya adalah suku " + suku + "dan bahasa sehari - hari saya kebanyakan adalah bahasa " + bahasa + " sekian dari saya " + salam);
+    }
+}
