ee7695dc1c39789ab22ff24fb431245bde72cd2e
Nero-Xrpl5 <sulungdinero@gmail.com>
2024-08-05 14:04:03 +0700

    Add files via upload

---
+++ Java Bu aul/JavaBuAul.java
@@ -0,0 +1,18 @@
+public class JavaBuAul{
+    public static void main(String[] args) {
+        String nama= "Dinero Sulung S.";
+        String asal= "Jember";
+        char jenisKelamin= 'L';
+        int usia= 15;
+        String kelas= "X RPL V";
+        String hobby= "Play Games";
+        System.out.println("Nama= "+nama);
+        System.out.println("Asal= "+asal);
+        System.out.println("Jenis Kelamin= "+jenisKelamin);
+        System.out.println("Usia= "+usia);
+        System.out.println("Kelas= "+kelas);
+        System.out.println("Hobby= "+hobby);
+
+    }
+
+}
